6f9238fd68ff9f568b17aa8544337bae521f0faa
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:29:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "spacelift" {
+  access_key = var.spacelift_access_key
+  secret_key = var.spacelift_secret_key
+}
