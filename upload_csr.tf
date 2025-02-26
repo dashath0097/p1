8f8a1cc68c010e4aa6ef9b9ddfa223c6aab16114
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:28:30 +0530

    Update upload_csr.tf

--- upload_csr.tf
+++ upload_csr.tf
@@ -1,10 +1,11 @@
 resource "null_resource" "upload_csr" {
   provisioner "local-exec" {
     command = <<EOT
-      curl -X POST "https://app.spacelift.io/api/v1/worker-pools/${spacelift_worker_pool.private_workers.id}/csr" \
-      -H "Authorization: Bearer ${var.spacelift_api_token}" \
-      -H "Content-Type: text/plain" \
-      --data-binary @worker.csr
+      SPACELIFT_ACCESS_KEY=${var.spacelift_access_key} \
+      SPACELIFT_SECRET_KEY=${var.spacelift_secret_key} \
+      spacelift worker-pool csr upload \
+      --worker-pool ${spacelift_worker_pool.private_workers.id} \
+      --csr-file worker.csr
     EOT
   }
 
