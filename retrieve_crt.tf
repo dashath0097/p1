417588eb2fbdb1770a19173eef92eaf335dec012
dashath0097 <dashathk0440@gmail.com>
2025-02-26 18:19:01 +0530

    Update retrieve_crt.tf

--- retrieve_crt.tf
+++ retrieve_crt.tf
@@ -1,9 +1,18 @@
 resource "null_resource" "fetch_worker_cert" {
   provisioner "local-exec" {
     command = <<EOT
+      # Ensure Spacelift CLI is installed
+      INSTALL_DIR="/usr/local/bin"
+      if [ ! -f "$INSTALL_DIR/spacelift-launcher" ]; then
+        echo "Installing Spacelift CLI..."
+        wget -O "$INSTALL_DIR/spacelift-launcher" https://downloads.spacelift.io/spacelift-launcher-x86_64
+        chmod +x "$INSTALL_DIR/spacelift-launcher"
+      fi
+
+      # Fetch Worker Certificate
       SPACELIFT_ACCESS_KEY=${var.spacelift_access_key} \
       SPACELIFT_SECRET_KEY=${var.spacelift_secret_key} \
-      spacelift worker-pool cert get \
+      "$INSTALL_DIR/spacelift-launcher" worker-pool cert get \
       --worker-pool ${spacelift_worker_pool.private_workers.id} \
       --output ${path.module}/worker.crt
     EOT
@@ -12,14 +21,15 @@ resource "null_resource" "fetch_worker_cert" {
   depends_on = [null_resource.upload_csr]
 }
 
+# Store the actual worker certificate contents
 resource "local_file" "worker_crt_file" {
-  content  = "Certificate generated and stored in worker.crt"
+  content  = file("${path.module}/worker.crt")  # Store actual certificate
   filename = "${path.module}/worker.crt"
 
   depends_on = [null_resource.fetch_worker_cert]
 }
 
 output "worker_cert" {
-  value     = "Certificate stored in worker.crt"
+  value     = file("${path.module}/worker.crt")  # Output actual certificate content
   sensitive = true
 }
