ca1097c60a05709539ac66acd7fa99b5eb31b8fb
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:28:46 +0530

    Update retrieve_crt.tf

--- retrieve_crt.tf
+++ retrieve_crt.tf
@@ -1,21 +1,20 @@
-data "http" "fetch_worker_cert" {
-  url = "https://app.spacelift.io/api/v1/worker-pools/${spacelift_worker_pool.private_workers.id}/certificate"
-
-  request_headers = {
-    Authorization = "Bearer ${var.spacelift_api_token}"
+resource "null_resource" "fetch_worker_cert" {
+  provisioner "local-exec" {
+    command = <<EOT
+      SPACELIFT_ACCESS_KEY=${var.spacelift_access_key} \
+      SPACELIFT_SECRET_KEY=${var.spacelift_secret_key} \
+      spacelift worker-pool cert get \
+      --worker-pool ${spacelift_worker_pool.private_workers.id} \
+      --output worker.crt
+    EOT
   }
 
   depends_on = [null_resource.upload_csr]
 }
 
 resource "local_file" "worker_crt_file" {
-  content  = data.http.fetch_worker_cert.body
+  content  = file("${path.module}/worker.crt")
   filename = "${path.module}/worker.crt"
 
-  depends_on = [data.http.fetch_worker_cert]
-}
-
-output "worker_cert" {
-  value     = data.http.fetch_worker_cert.body
-  sensitive = true
+  depends_on = [null_resource.fetch_worker_cert]
 }
