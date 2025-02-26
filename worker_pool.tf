ef88f473df3545d722560702b4b6895c964f5ac3
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:34:41 +0530

    Update worker_pool.tf

--- worker_pool.tf
+++ worker_pool.tf
@@ -1,7 +1,3 @@
-provider "spacelift" {
-  access_key = var.spacelift_access_key
-  secret_key = var.spacelift_secret_key
-}
 
 resource "spacelift_worker_pool" "private_workers" {
   name        = "private-ec2-workers"
