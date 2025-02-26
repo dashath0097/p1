04dc21d350abcd6f2508533d75c9b5f4359ba31d
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:29:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "spacelift_access_key" {
+  description = "Spacelift API Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "spacelift_secret_key" {
+  description = "Spacelift API Secret Key"
+  type        = string
+  sensitive   = true
+}
