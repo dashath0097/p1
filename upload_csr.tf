d4d24551b659d830333770a223224aa8651ad055
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:55:06 +0530

    Update upload_csr.tf

--- upload_csr.tf
+++ upload_csr.tf
@@ -1,13 +1,19 @@
+resource "null_resource" "install_spacelift_cli" {
+  provisioner "local-exec" {
+    command = "bash ${path.module}/install_spacelift.sh"
+  }
+}
+
 resource "null_resource" "upload_csr" {
   provisioner "local-exec" {
     command = <<EOT
       SPACELIFT_ACCESS_KEY=${var.spacelift_access_key} \
       SPACELIFT_SECRET_KEY=${var.spacelift_secret_key} \
-      spacelift worker-pool csr upload \
+      /usr/local/bin/spacelift worker-pool csr upload \
       --worker-pool ${spacelift_worker_pool.private_workers.id} \
       --csr-file worker.csr
     EOT
   }
 
-  depends_on = [local_file.worker_csr_file]
+  depends_on = [null_resource.install_spacelift_cli] # Ensure CLI is installed first
 }
