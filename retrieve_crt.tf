247f2730802fd37bd34372314372c84b3a4b2907
dashath0097 <dashathk0440@gmail.com>
2025-02-26 11:47:58 +0530

    Update retrieve_crt.tf

--- retrieve_crt.tf
+++ retrieve_crt.tf
@@ -5,7 +5,7 @@ resource "null_resource" "fetch_worker_cert" {
       SPACELIFT_SECRET_KEY=${var.spacelift_secret_key} \
       spacelift worker-pool cert get \
       --worker-pool ${spacelift_worker_pool.private_workers.id} \
-      --output worker.crt
+      --output ${path.module}/worker.crt
     EOT
   }
 
@@ -13,13 +13,13 @@ resource "null_resource" "fetch_worker_cert" {
 }
 
 resource "local_file" "worker_crt_file" {
-  content  = file("${path.module}/worker.crt")
+  content  = "Certificate generated and stored in worker.crt"
   filename = "${path.module}/worker.crt"
 
   depends_on = [null_resource.fetch_worker_cert]
 }
 
 output "worker_cert" {
-  value     = file("${path.module}/worker.crt")
+  value     = "Certificate stored in worker.crt"
   sensitive = true
 }
